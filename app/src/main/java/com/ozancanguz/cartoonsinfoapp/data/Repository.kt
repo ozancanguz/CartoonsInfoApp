b2c67fd47abb184c6a22070340528502a506558c
Ozan <ozancangu24@gmail.com>
2022-12-14 18:34:06 +0300

    -repository injected with localdatasource

--- app/src/main/java/com/ozancanguz/cartoonsinfoapp/data/Repository.kt
+++ app/src/main/java/com/ozancanguz/cartoonsinfoapp/data/Repository.kt
@@ -1,5 +1,6 @@
 package com.ozancanguz.cartoonsinfoapp.data
 
+import com.ozancanguz.cartoonsinfoapp.data.localdatasource.LocalDataSource
 import com.ozancanguz.cartoonsinfoapp.data.remote.RemoteDataSource
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.scopes.ViewModelScoped
@@ -7,9 +8,12 @@ import javax.inject.Inject
 
 
 @ViewModelScoped
-class Repository@Inject constructor(remoteDataSource: RemoteDataSource){
+class Repository@Inject constructor(remoteDataSource: RemoteDataSource,
+                        localDataSource: LocalDataSource
+                                    ){
 
     var remote=remoteDataSource
+    var local=localDataSource
 
 
 }
